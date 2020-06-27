cd5712d6bebc702c202162795b39ee1316429200
Seth Broughton <broughton.24@gmail.com>
2020-06-27 10:02:08 -0400

    Update birthday.sql

--- birthdaywish/Database/birthday.sql
+++ birthdaywish/Database/birthday.sql
@@ -12,7 +12,7 @@ CREATE TABLE birthday
 );
 
 INSERT INTO birthday (first_name, last_name, date_of_birth, email, phone_number)
-VALUES ('Meg', 'Keegan Broughton', '1989-02-24', 'mkeegan89@gmail.com', '7403361202'),
-        ('Seth','Broughton','1988-01-07','broughton.24@gmail.con','6145808483');
+VALUES ('Meg', 'Keegan Broughton', '1987-02-02', 'mkeegan89@gmail.com', '5555555555'),
+        ('Seth','Broughton','1990-01-05','broughton.24@gmail.con','5555555555');
 
 
